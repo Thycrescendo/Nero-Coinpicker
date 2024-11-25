aaf9fe3999437daeac5f9edefe2eed88200224c7
thycrescendo <dmicholn@gmail.com>
2024-11-25 20:23:02 +0100

    changed index and configuraton

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 
-);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist/index.html"));
