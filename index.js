38037c1956513053c604a74f6ed51fcef58e15f8
thycrescendo <dmicholn@gmail.com>
2024-11-25 20:22:27 +0100

    changed index and configuraton

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ setTimeout(() => pubsub.broadcastChain(), 1500);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client/dist")));
 
-require("./routes/blockchain")(app, blo
+require("./routes/blockchain")(app, 
 );
 
 app.get("*", (req, res) => {
