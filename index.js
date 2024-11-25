71393698edea1ce898845ad041e7daac90bbe77b
thycrescendo <dmicholn@gmail.com>
2024-11-25 20:22:18 +0100

    changed index and configuraton

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 require("./routes/blockchain")(app, blockchain, pubsub);
-require("./routes/
+
 );
 
 app.get("*", (req, res) => {
